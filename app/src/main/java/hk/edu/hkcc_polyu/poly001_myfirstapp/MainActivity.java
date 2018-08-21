229bcb2077d7063032de40b80202118ebcd74197
john <ngkaipong1c24@gmail.com>
2018-08-21 17:47:51 +0800

    initial commit

---
+++ app/src/main/java/hk/edu/hkcc_polyu/poly001_myfirstapp/MainActivity.java
@@ -0,0 +1,62 @@
+package hk.edu.hkcc_polyu.poly001_myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create team1
+        Player player1 = new Player();
+        player1.playerName = "George Eliot";
+        Player player2 = new Player();
+        player2.playerName = "Graham Greene";
+        Player player3 = new Player();
+        player3.playerName = "Geoffrey Chaucer";
+        Player[] thePlayers = {player1, player2, player3 };
+
+        Team team1 = new Team();
+        team1.teamName = "The Greens";
+        team1.playerArray = thePlayers;
+
+        // Create team2
+        Team team2 = new Team();
+        team2.teamName = "The Reds";
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].playerName = "Robert Service";
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].playerName = "Robbie Burns";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].playerName = "Rafael Sabatini";
+
+
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+        Goal[] theGoals = {goal1};
+        currGame.goals = theGoals;
+
+        System.out.println("Goal scored after " +
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+
+        TextView tv1 = (TextView) findViewById(R.id.textView);
+        
+        /* Practice 6-2. Print out the score of the Game */
+        tv1.setText("Goal scored after " +
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+    }
+}
---
+++ app/src/main/java/hk/edu/hkcc_polyu/poly001_myfirstapp/Team.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hk.edu.hkcc_polyu.poly001_myfirstapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Team {
+    
+    public String teamName;
+    public Player[] playerArray;
+    
+}
